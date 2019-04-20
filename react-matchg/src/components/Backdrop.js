65ae9ccb9b310f90f585b9dd957ac6a15e6028fb
JLasc <lascaze.j@gmail.com>
2019-04-20 14:49:16 -0400

    custom font-size

--- react-matchg/src/components/Backdrop.js
+++ react-matchg/src/components/Backdrop.js
@@ -7,8 +7,55 @@ class Backdrop extends Component {
     render() {
         return (
             <div className='backdrop-style'>
-                <div className='card-holder'>
-                    <Card />
+                <div className='card-holder container'>
+                    <div class='row'>
+                        <div class='col-xs-3'>
+                            <img src='https://via.placeholder.com/160' />
+                        </div>
+                        <div class='col-xs-3'>
+                            <img src='https://via.placeholder.com/160' />
+                        </div>
+
+                        <div class='col-xs-3'>
+                            <img src='https://via.placeholder.com/160' />
+                        </div>
+
+                        <div class='col-xs-3'>
+                            <img src='https://via.placeholder.com/160' />
+                        </div>
+                    </div>
+                    <div class='row'>
+                        <div class='col-xs-3'>
+                            <img src='https://via.placeholder.com/160' />
+                        </div>
+                        <div class='col-xs-3'>
+                            <img src='https://via.placeholder.com/160' />
+                        </div>
+
+                        <div class='col-xs-3'>
+                            <img src='https://via.placeholder.com/160' />
+                        </div>
+
+                        <div class='col-xs-3'>
+                            <img src='https://via.placeholder.com/160' />
+                        </div>
+                    </div>
+                    <div class='row'>
+                        <div class='col-xs-3'>
+                            <img src='https://via.placeholder.com/160' />
+                        </div>
+                        <div class='col-xs-3'>
+                            <img src='https://via.placeholder.com/160' />
+                        </div>
+
+                        <div class='col-xs-3'>
+                            <img src='https://via.placeholder.com/160' />
+                        </div>
+
+                        <div class='col-xs-3'>
+                            <img src='https://via.placeholder.com/160' />
+                        </div>
+                    </div>
                 </div>
             </div>
         )
