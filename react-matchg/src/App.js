f0fa3041f8e1ddd75a50905b6c1d419f8ce4a8ad
JLasc <lascaze.j@gmail.com>
2019-04-20 18:30:37 -0400

    refactored to include ImageCard render for state items

--- react-matchg/src/App.js
+++ react-matchg/src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import Header from './components/Header'
 import Footer from './components/Footer'
-import ImageCard from './components/ImageCard'
-import data from './assets/scripts/list.json'
+import data from './assets/json/list.json'
 import './assets/css/style.css'
 
 class App extends Component {
@@ -12,11 +11,31 @@ class App extends Component {
     dataArr: data
   }
 
+  componenetDidMount() {
+
+  }
+
+  handleClick = id => {
+    const imageId = this.state.dataArr
+    const clickedImage = imageId.filter(data => data.id === id)
+    console.log(clickedImage)
+  }
+
   render() {
+    const ImageCard = this.state.dataArr.map((item, index) => {
+      return (
+        <div className='container drop'>
+          <img className='img-thumbnail' onClick={() => this.handleClick(item.id)} key={index} src={item.src} alt="" />
+        </div>
+      )
+    });
+
     return (
       <div>
         <Header score={this.state.score} currScore={this.state.currScore} />
-        <ImageCard data={data} />
+        <div className='container drop'>
+          {ImageCard}
+        </div>
         <Footer />
       </div>
     );
