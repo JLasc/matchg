4e74a0103d2acdf96af310fa257844e3faf09716
JLasc <lascaze.j@gmail.com>
2019-04-20 13:41:38 -0400

    render components

--- react-matchg/src/App.js
+++ react-matchg/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
+import Header from './components/Header'
+import Backdrop from './components/Backdrop'
+import './assets/css/style.css'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello world</h1>
+        <Header />
+        <Backdrop />
       </div>
     );
   }
