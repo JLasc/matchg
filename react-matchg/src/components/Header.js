9e5ec8c9dc3c5be5388aa209f39a9e4541b3f609
JLasc <lascaze.j@gmail.com>
2019-04-20 13:42:05 -0400

    header component

---
+++ react-matchg/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+
+const Header = () => {
+
+    return (
+        <header className='header-style'>
+            <section className='header-title'>MATCHING GAME</section>
+        </header>
+    )
+};
+
+
+export default Header;
