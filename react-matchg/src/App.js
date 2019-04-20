2affbb6c0095cbdec0486fcf08bf4c66616be54e
JLasc <lascaze.j@gmail.com>
2019-04-20 16:43:42 -0400

    state creation

--- react-matchg/src/App.js
+++ react-matchg/src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
-import Backdrop from './components/Backdrop'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Backdrop from './components/Backdrop'
 import './assets/css/style.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    state: {
+
+    }
+  }
+
 
   render() {
     return (
