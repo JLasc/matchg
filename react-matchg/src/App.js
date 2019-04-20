a52faff9eb208996b2690b4580600205f44c996d
JLasc <lascaze.j@gmail.com>
2019-04-20 14:49:05 -0400

    added footer component

--- react-matchg/src/App.js
+++ react-matchg/src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import Header from './components/Header'
 import Backdrop from './components/Backdrop'
+import Header from './components/Header'
+import Footer from './components/Footer'
 import './assets/css/style.css'
 
 class App extends Component {
+
   render() {
     return (
       <div>
         <Header />
         <Backdrop />
+        <Footer />
       </div>
     );
   }
