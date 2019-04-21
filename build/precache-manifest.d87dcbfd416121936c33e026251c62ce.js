1312a27d13c5063f8cb00af0a004a981416ecd3d
JLasc <lascaze.j@gmail.com>
2019-04-20 22:54:25 -0400

    update

---
+++ build/precache-manifest.d87dcbfd416121936c33e026251c62ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b55693ff0755547018d1",
+    "url": "/matchg/static/js/runtime~main.e87327da.js"
+  },
+  {
+    "revision": "71af0ab782f4a13f8373",
+    "url": "/matchg/static/js/main.48fa0566.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/matchg/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "71af0ab782f4a13f8373",
+    "url": "/matchg/static/css/main.545f1e76.chunk.css"
+  },
+  {
+    "revision": "624ba4c1f1402b919d4648176f99c902",
+    "url": "/matchg/index.html"
+  }
+];
