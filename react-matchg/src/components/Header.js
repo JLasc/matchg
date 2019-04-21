11c4ad5be765c64ca87eb209ee61f638da1c9d70
JLasc <lascaze.j@gmail.com>
2019-04-20 22:15:10 -0400

    updated text

--- react-matchg/src/components/Header.js
+++ react-matchg/src/components/Header.js
@@ -5,12 +5,12 @@ const Header = props => {
         <div>
             <nav className="navbar navbar-light justify-content-between header-navbg">
                 <a className="navbar-brand navFontSize header-match-title header-font-size" > Matching Game</a>
-                <span className='mr-sm-1 navFontSize header-font-size'>Click an image to begin!</span>
-                <span className='mr-sm-2 navFontSize header-font-size'>Score: {props.score} | Top Score: {props.currScore} </span>
+                <span className='mr-sm-2 navFontSize header-font-size'>Score: {props.score} | Top Score: {props.topScore} </span>
             </nav>
             <div class="jumbotron jumbotron-fluid text-white jumboBg">
                 <div class="container text-center">
-                    <p class="lead">Click on an image to earn points! Do not click the same picture twice.</p>
+                    <p class="lead">Click on an image to earn points!</p>
+                    <p>Do not click the same picture twice.</p>
                 </div>
             </div>
         </div>
