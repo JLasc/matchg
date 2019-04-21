b81545ceff31a643fd932c14287fcca62289b363
JLasc <lascaze.j@gmail.com>
2019-04-20 22:13:42 -0400

    refactored methods, working alerts

--- react-matchg/src/App.js
+++ react-matchg/src/App.js
@@ -1,27 +1,100 @@
 import React, { Component } from 'react';
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Alert from './components/Alert'
 import data from './assets/json/list.json'
+import './assets/scripts/scripts'
 import './assets/css/style.css'
+
 
 class App extends Component {
   state = {
     score: 0,
-    currScore: 0,
-    dataArr: data
+    topScore: 0,
+    dataArr: data,
+    isHidden: true,
+    isWrong: false
   }
 
-  componenetDidMount() {
+  shuffle = array => {
+    let currentIndex = array.length, temporaryValue, randomIndex
 
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex]
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+
+  componentWillMount() {
+    this.shuffle(data)
+  }
+  toggleHidden = () => {
+    this.setState({ isHidden: !this.state.isHidden })
+  }
+
+  toggleHiddenWrong = () => {
+    this.setState({ isWrong: !this.state.isWrong })
+  }
+
+  endGame = () => {
+    const reset = this.state.dataArr
+
+    if (this.state.topScore > this.state.score) {
+      //reset json values to clicked: false
+      reset.forEach(item => { item.clicked = false; })
+      this.setState({ score: 0 })
+      this.shuffle(data)
+    } else {
+      reset.forEach(item => { item.clicked = false; })
+      this.setState({ score: 0, topScore: this.state.score })
+      this.shuffle(data)
+    }
+  }
+
+  toggleHidden = () => {
+    this.setState({ isHidden: !this.state.isHidden })
+  }
+
+  toggleHiddenWrong = () => {
+    this.setState({ isWrong: !this.state.isWrong })
+  }
+
+
+  winListener = () => {
+    if (this.state.isHidden === false) {
+      setTimeout(this.toggleHidden, 2000)
+    }
+
+    else if (this.state.isWrong === true) {
+      setTimeout(this.toggleHiddenWrong, 2000)
+    }
   }
 
   handleClick = id => {
     const imageId = this.state.dataArr
     const clickedImage = imageId.filter(data => data.id === id)
-    console.log(clickedImage)
+
+    if (clickedImage[0].clicked === false) {
+      clickedImage[0].clicked = true
+      this.setState({
+        score: this.state.score += 1,
+      })
+      this.shuffle(data)
+
+    } else if (this.state.score === 12) {
+      this.setState({ score: 0, topScore: 12 })
+      this.toggleHidden()
+
+    } else {
+      this.toggleHiddenWrong()
+      this.endGame()
+    }
+
   }
-
-
 
   render() {
     const ImageCard = this.state.dataArr.map((item, index) => {
@@ -34,7 +107,10 @@ class App extends Component {
 
     return (
       <div>
-        <Header score={this.state.score} currScore={this.state.currScore} />
+        <Header score={this.state.score} topScore={this.state.topScore} />
+        {this.winListener()}
+        {!this.state.isHidden && <Alert.successAlert />}
+        {this.state.isWrong && <Alert.dangerAlert />}
         <div className='container drop'>
           {ImageCard}
         </div>
@@ -43,5 +119,6 @@ class App extends Component {
     );
   }
 }
+
 
 export default App;
